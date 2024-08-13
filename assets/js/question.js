5990125570e3d725879c753734a640b162129b38
IperferTunde <Foltunde@gmail.com>
2024-08-13 17:23:13 +0000

    set of question for question.js

--- assets/js/question.js
+++ assets/js/question.js
@@ -1,7 +1,7 @@
 // This is the question funstions that contain questions and the answers. They are in multidimensional array with inner array elements
 var questions = [
     { 
-        question: "How do you write 'Hello World' in an alert box?", 
+        question: "What is your country of birth?", 
         answers: [
             { text: "msg('Hello World')", correct: false },
             { text: "alert('Hello World')" , correct: true },
@@ -9,40 +9,36 @@ var questions = [
             { text: "alertBox('Hello World')", correct: false }
         ]
     },
+
     { 
-        question: "Which of the following function of Array object calls a function for each element in the array?", 
+        question: "What is your gender?", 
         answers: [
-            { text: "concat()", correct: false },
-            { text: "filter()", correct: false },
-            { text: "forEach()", correct: true },
-            { text: "split()", correct: false }
+            { text: "Male", correct: true },
+            { text: "Female", correct: true },
         ]
     },
     { 
-        question: "How to write an IF statement for executing some code if 'i' is NOT equal to 8?", 
+        question: "Are you married?", 
         answers: [
-            { text: "if (i != 8)", correct: true },
-            { text: "if i =! 8", correct: false },
-            { text: "if (i <> 8)", correct: false },
-            { text: "if (i !=== 8)", correct: false }
+            { text: "No", correct: false },
+            { text: "Yes", correct: true },
+            { text: "Complicated", correct: false },
         ]
     },
     { 
-        question: "What is the correct way to write a JavaScript array?", 
+        question: "Do you have children?",
         answers: [
-            { text: "var vegetable = (0:'kale', 1:'lettuce', 2:'spinach')", correct: false },
-            { text: "var vegetable = ['kale', 'lettuce', 'spinach']", correct: true },
-            { text: "var vegetable = (kale, lettuce, spinach)", correct: false },
-            { text: "None of the above", correct: false }
+            { text: "Yes", correct: false },
+            { text: "NO", correct: false },
         ]
     },
     { 
-        question: "How do you round the number 102.456, to the nearest integer?",
+        question: "investment option?",
         answers: [
-            { text: "Math.random(102.456)", correct: false },
-            { text: "Math.rnd(102.456)", correct: false },
-            { text: "round(102.456)", correct: false },
-            { text: "None of the above", correct: true }
+            { text: "Golden Visa", correct: false },
+            { text: "Golden Visa Lativa", correct: false },
+            { text: "Citizenship by investment", correct:true}
+            { text: "Real Estate project", correct:true}
         ]
     },
 ];
